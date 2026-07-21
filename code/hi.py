c5b9d76fe28a8a6e9894ec2a0eb0530686625c43
badhanlovely75-del <badhanlovely75@gmail.com>
2026-07-21 12:43:14 +0530

    first commit

---
+++ code/hi.py
@@ -0,0 +1,62 @@
+# import math 
+# num = 66666
+# root = math.sqrt(num)
+# print(root)
+
+# import random
+# list1=["lovely","name","age"]
+# list2=["lovely","name","age"]
+
+# x= random.randint(1,20)
+# print(x)
+# print(random.choice(list1))
+# print(random.choices(list2))
+# print(list1)
+# print(list2)
+# print(type["list1"])
+# print(type["list2"])
+# import random 
+
+
+
+
+
+
+
+
+
+# list =["s","p","s"]
+
+# cs=random.choice(list)
+# print(cs)
+
+
+
+
+
+
+
+
+# import math
+
+# print(math.sqrt(25)
+
+
+
+
+# list =["s","p","s"]
+
+# 9=random.choice(list)
+# print(9)
+
+
+
+# list =["s","p","s"]
+
+# cs=random.choice(list)
+# print(cs) 
+
+
+# import circle
+# x=d=10cm
+# print(x)
---
+++ code/lovely.py
@@ -0,0 +1,41 @@
+# with open("hi.txt","x")as lovely:
+#     data="student list "
+#     lovely.write(data) 
+
+
+# with open("hi.txt","")as lovely:
+#     data="student list "
+#     lovely.write(data)                                                           
+
+
+
+
+
+# with open("hi.txt", "rb") as file:
+#     binary_data = file.read()
+#     print(binary_data[:10])  
+
+
+
+
+
+
+
+
+# with open("image.jpg", "rb") as file:
+#     data = file.read()
+
+# with open("copy.jpg", "wb") as file:
+#     file.write(data)  
+
+# with open("copy.jpg", "wb") as file:
+#     file.write(data)
+
+
+
+
+# file = open("hi.txt", "a")
+
+# file.write("\n next line ""\n next line""\n new line")
+
+# file.close()
